e5afb23493a56ca78b618e7f45e598b2b70ad153
Ilya Goss <igossoudarev@herzen.spb.ru>
2017-10-06 04:13:16 +0300

    Update z7a.mjs

--- z7a.mjs
+++ z7a.mjs
@@ -1,5 +1,5 @@
 import Zombie from 'zombie';
-const URL = 'https://floating-mountain-40491.herokuapp.com/';
+const URL = process.argv[2] || 'https://kodaktor.ru/g/boo'  // https://infinite-lake-15917.herokuapp.com/';
 const page = new Zombie();
 const g=URL=>new Promise((rs,rj)=>page.visit(URL,e=>(e)?rj(e):rs()));
 (async()=>{
