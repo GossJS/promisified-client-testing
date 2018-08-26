9ef7902cdbbbd0a3c8d8aaeb76d47b5fa6bba966
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-08-26 17:39:56 +0300

    Create case.spec.js

---
+++ test/case.spec.js
@@ -0,0 +1,23 @@
+const should = require('should');
+const Zombie = require('zombie');
+const page  = new  Zombie();
+
+const cases = [
+    {s: 'abc', xs: 'CBA'}, 
+    {s: 'mob', xs: 'BOM'}, 
+    {s: '123', xs: '321'},
+    {s: 'omen', xs: 'NEMO'}
+];
+
+const URL = 'https://kodaktor.ru/g/autocase';        
+     
+cases.forEach(({ s, xs }) =>
+    describe(xs + ' asyncREV()', () => 
+      it('respond with revers', async () => {
+        await page.visit(URL);  
+        page.fill('#inp', s);
+        page.pressButton('#button_do'); 
+        page.document.querySelector('#ans').value.should.equal(xs);
+      })
+    )
+);
