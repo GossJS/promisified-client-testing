3043846c26e8b02e1c79803b848b806ee7af33e1
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-08-26 17:38:02 +0300

    Delete z7a.mjs

--- z7a.mjs
+++
@@ -1,9 +0,0 @@
-import Zombie from 'zombie';
-const URL = process.argv[2] || 'https://kodaktor.ru/g/boo'  // https://infinite-lake-15917.herokuapp.com/';
-const page = new Zombie();
-const g=URL=>new Promise((rs,rj)=>page.visit(URL,e=>(e)?rj(e):rs()));
-(async()=>{
-    await g(URL);
-    console.log(page.document.querySelector('h4#author').title==='GossJS' );
-    console.log(page.document.querySelector('h4#author').textContent );
-})();
