4282f6f2a34a55cd771f4c9bcdfe2de42821b3bc
Ilya Goss <goss@corp.ifmo.ru>
2020-03-27 07:22:25 +0300

    Update case.spec.js

--- test/case.spec.js
+++ test/case.spec.js
@@ -3,17 +3,17 @@ const Zombie = require('zombie');
 const page  = new  Zombie();
 
 const cases = [
-    {s: 'abc', xs: 'CBA'}, 
-    {s: 'mob', xs: 'BOM'}, 
-    {s: '123', xs: '321'},
-    {s: 'omen', xs: 'NEMO'}
+    { s: 'abc', xs: 'CBA' }, 
+    { s: 'mob', xs: 'BOM' }, 
+    { s: '123', xs: '321' },
+    { s: 'omen', xs: 'NEMO' }
 ];
 
 const URL = 'https://kodaktor.ru/g/autocase';        
      
 cases.forEach(({ s, xs }) =>
     describe(xs + ' asyncREV()', () => 
-      it('respond with revers', async () => {
+      it('responds with revers', async () => {
         await page.visit(URL);  
         page.fill('#inp', s);
         page.pressButton('#button_do'); 
